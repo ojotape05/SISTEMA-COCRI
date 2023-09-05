ef1e68a79c4003c745b38732b76fa701886715ab
ojotape05 <jpbpissineli05@gmail.com>
2023-09-05 17:57:27 -0300

    inserts (excp venda)

--- backend/database/db.js
+++ backend/database/db.js
@@ -33,22 +33,52 @@ async function selectAll(table) {
     }
 }
 
-async function insertMutuario(data) {
+async function insertVendedor(data) {
+    console.log("vend:",data)
     try{
         const client = await connect();
-        const sql = 'INSERT INTO mutuario(num_mutuario,nome,tipo) VALUES ($1,$2,$3);';
-        const values = [data.num_mutuario, data.nome, data.tipo];
+        const sql = 'INSERT INTO vendedor(nome_vend,num_vend,id_processo) VALUES ($1,$2,$3);';
+        const values = [data.nome_vend,data.num_vend,data.id_processo];
         return await client.query(sql, values);
     }catch(e){
+        console.log(e)
         return e;
     }
     
 }
 
+async function insertMutuario(data) {
+    console.log("mutuario:",data)
+    try{
+        const client = await connect();
+        const sql = 'INSERT INTO mutuario(num_mut,nome_mut,dt_nascimento_mut,id_processo) VALUES ($1,$2,$3,$4 RETURNING cpf_cnpj;';
+        const values = [data.num_mut, data.nome_mut, data.dt_nascimento_mut,data.id_processo];
+        const cpf_cnpj = (await client.query(sql, values)).rows[0].cpf_cnpj
+        return cpf_cnpj
+    }catch(e){
+        console.log(e)
+        return e;
+    }
+    
+}
+
+async function insertCompra(id_processo,cpf_cnpj){
+    try{
+        const client = await connect();
+        const sql = 'INSERT INTO compra(mut_cpf_cnpj,id_processo) VALUES($1,$2)'
+        const values = [cpf_cnpj,id_processo]
+        return await client.query(sql, values)
+    }
+    catch(e){
+        console.log("insertCompra:",e)
+        return e
+    }
+}
+
 async function insertAbertura(data) {
     try{
         const client = await connect();
-        const sql = 'INSERT INTO processo(dt_entrada_gecri,vl_compra_venda,vl_financiamento,fgts,iq,sist_amortizacao,prazo,taxa,cid,nome_agencia,sureg,cnpj_corresp,nome_corresp) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13);';
+        const sql = 'INSERT INTO processo(dt_entrada_gecri,vl_compra_venda,vl_financiamento,fgts,iq,sist_amortizacao,prazo,taxa,tipo_taxa,cid,nome_agencia,sureg,cnpj_corresp,nome_corresp,operacao) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) RETURNING id_processo;';
         const values = [
             data.dt_entrada_gecri,
             data.vl_compra_venda,
@@ -58,13 +88,26 @@ async function insertAbertura(data) {
             data.sist_amortizacao,
             data.prazo,
             data.taxa,
+            data.tipo_taxa,
             data.agencia.cid,
             data.agencia.nome_agencia,
             data.agencia.sureg,
             data.corresp.cnpj_correspondente,
-            data.corresp.nome_correspondente
+            data.corresp.nome_correspondente,
+            data.operacao
         ];
-        return await client.query(sql, values);
+        const id_processo = (await client.query(sql, values)).rows[0].id_processo
+        console.log(id_processo)
+        for(mut of data.mutuarios){
+            mut.id_processo = id_processo
+            const cpf_cnpj = insertMutuario(mut)
+            insertCompra(id_processo,cpf_cnpj)
+        }
+        for(vend of data.vendedores){
+            vend.id_processo = id_processo
+            insertVendedor(vend)
+        }
+        return {status:202};
     }catch(e){
         console.log(e)
         return e;
--- frontend/js/abertura_processo/post_infos.js
+++ frontend/js/abertura_processo/post_infos.js
@@ -4,7 +4,7 @@ if (document.querySelector('#aberturaProcesso')) {
     elem.addEventListener("submit", function (event) {
       event.preventDefault();
 
-      tipoTaxa = document.querySelectorAll("[name=tipoTaxa]").forEach( el => {
+      tipo_taxa = document.querySelectorAll("[name=tipoTaxa]").forEach( el => {
         if(el.checked) return el.value
       })
 
@@ -22,7 +22,7 @@ if (document.querySelector('#aberturaProcesso')) {
       var vendedores = []
       for(var i = 0; i < numVendedores; i++){
         vendedores.push({
-          nome_vendedor: document.querySelectorAll('[name="vendedor"]')[i].value,
+          nome_vend: document.querySelectorAll('[name="vendedor"]')[i].value,
           num_vend: document.querySelectorAll('[name="num_Vendedor"]')[i].value
         })
       }
@@ -34,17 +34,17 @@ if (document.querySelector('#aberturaProcesso')) {
         fgts: document.getElementById("fgts").checked,
         iq: document.getElementById("iq").checked,
         sist_amortizacao: document.getElementById("tipodeamortizacao").value,
-        prazo: document.getElementById("prazo").value,
+        prazo: parseInt(document.getElementById("prazo").value),
         taxa: parseFloat((document.getElementById("taxa").value).replace(/[^0-9,]+/gm,"").replace(",",".")),
-        tipoTaxa: tipoTaxa,
+        tipo_taxa: tipo_taxa,
         agencia: {
           cid: document.getElementById("cid").value,
           nome_agencia: document.getElementById("agencia").value,
           sureg: document.getElementById("superintendencia").value
         },
         corresp: {
-          nome_correspodente: document.getElementById("nomeCorrespondente").value,
-          cnpj_correspondente: document.getElementById("cnpjCorrespondente").value
+          nome_corresp: (document.getElementById("nomeCorrespondente").value != '' ? document.getElementById("nomeCorrespondente").value : null),
+          cnpj_corresp: (document.getElementById("cnpjCorrespondente").value != '' ? document.getElementById("cnpjCorrespondente").value : null)
         },
         operacao: document.querySelector("#tipodeoperacao").value,
         mutuarios,
